42fa5e01fc8a1459bb08f9eac23a78f02e264f96
crayon <15651875728@163.com>
2021-01-01 16:00:00 +0800

    :sparkles: 2020-12-29 21-easy

---
+++ 24.两两交换链表中的节点.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=cpp
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head) return head;
+        if (!head->next) return head;
+        
+        vector<ListNode*> pList;
+        for (ListNode *p = head; p; p = p->next) pList.push_back(p);
+        
+        int len = pList.size();
+        for (int i = 0; i < len; i += 2) {
+            ListNode *cur = pList[i];
+            if (cur->next) cur->next->next = cur;
+        }
+
+        for (int i = 0; i < len; i += 2) {
+            ListNode *cur = pList[i];
+            if (i + 3 < len) cur->next = pList[i+3];
+            else if (i + 2 < len) cur->next = pList[i+2];
+            else cur->next = nullptr;
+        }
+
+        return pList[1];
+    }
+};
+// @lc code=end
+
---
+++ 25.k-个一组翻转链表.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=25 lang=cpp
+ *
+ * [25] K 个一组翻转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head) return head;
+        if (!head->next) return head;
+        
+        vector<ListNode*> pList;
+        for (ListNode *p = head; p; p = p->next) pList.push_back(p);
+        
+        int len = pList.size();
+        for (int i = 0; i + k - 1 < len; i += k) {
+            for (int j = i + k - 1; j >= i + 1; j--) {
+                pList[j]->next = pList[j-1];
+            }
+        }
+
+        for (int i = 0; i + k - 1 < len; i += k) {
+            if (i + k + k - 1 < len) pList[i]->next = pList[i + k + k - 1];
+            else if (i + k < len) pList[i]->next = pList[i + k];
+            else pList[i]->next = nullptr;
+        }
+
+        return pList[k-1];
+    }
+};
+// @lc code=end
+
