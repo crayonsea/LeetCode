7e2e4668c8436bde98a4432730b9fb0cce233c96
crayon <15651875728@163.com>
2021-01-11 14:57:29 +0800

    :art: 2021-01-11 medium-15

--- 15.三数之和.cpp
+++ 15.三数之和.cpp
@@ -16,19 +16,33 @@ public:
         for (int i = 0; i < len - 2 && nums[i] <= 0; i++) {
             if (i > 0 && nums[i] == nums[i-1]) continue;
 
-            int k = len - 1;
-            // k 是可以在 j 循环过程中累计变化的! 因为 随着 j↑, 需要 k↓, k
-            for (int j = i + 1; j < len - 1 && nums[i] + nums[j] <= 0; j++) {
-                if (j > i + 1 && nums[j] == nums[j-1]) continue;
+            // int k = len - 1;
+            // // k 是可以在 j 循环过程中累计变化的! 因为 随着 j↑, 需要 k↓, k
+            // for (int j = i + 1; j < len - 1 && nums[i] + nums[j] <= 0; j++) {
+            //     if (j > i + 1 && nums[j] == nums[j-1]) continue;
 
+            //     while (j < k && nums[i] + nums[j] + nums[k] > 0) k--;
+            //     if (j == k) break;
+
+            //     // 因为 nums[j] 一定会增大 (continue 语句)
+            //     // 所以不需要显示比较, nums[k] 是否和上一次 (nums[k+1]) 相等
+            //     if (nums[i] + nums[j] + nums[k] == 0) {
+            //         ans.push_back({nums[i], nums[j], nums[k]});
+            //     }
+            // }
+
+            int j = i + 1, k = len - 1;
+            while (j < k) {
+                // jump same <j>
+                while (j < k && j > i + 1 && nums[j] == nums[j-1]) j++;
+                // iter <k>
                 while (j < k && nums[i] + nums[j] + nums[k] > 0) k--;
-                if (j == k) break;
-
-                // 因为 nums[j] 一定会增大 (continue 语句)
-                // 所以不需要显示比较, nums[k] 是否和上一次 (nums[k+1]) 相等
-                if (nums[i] + nums[j] + nums[k] == 0) {
+                // add ans
+                if (j  < k && nums[i] + nums[j] + nums[k] == 0) {
                     ans.push_back({nums[i], nums[j], nums[k]});
                 }
+                // iter <j>
+                j++;
             }
         }
        
